9a2eebc26f59cfe3dac199c0c58127241f0e37de
VirlanValentin <stefan.virlan@info.uaic.ro>
2015-05-23 17:30:34 +0300

    CRUD Subdomain

--- ASPHomework/ASPHomework/Repositories/DSQHelper.cs
+++ ASPHomework/ASPHomework/Repositories/DSQHelper.cs
@@ -2,10 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DataBase;
 
 namespace ASPHomework.Repositories
 {
     public class DSQHelper
     {
+        //SubdomainHelper
+        //GetAll
+        public static List<Subdomains> GetAllSubdomains()
+        {
+            using (var context = new ervinEntities())
+            {
+                return context.Subdomains.ToList();
+            }
+        }
+  
+        //Add
+        public static void AddSubdomain(Subdomains subdomain)
+        {
+            using (var context = new ervinEntities())
+            {
+                context.Subdomains.Add(subdomain);
+                context.SaveChanges();
+            }
+        }
+        //Edit
+        public static void EditSubdomain(Subdomains subdomain)
+        {
+            using (var context = new ervinEntities())
+            {
+                var selectedSubdomain = context.Subdomains.First(x => x.Id == subdomain.Id);
+                selectedSubdomain.IdDomain = subdomain.IdDomain;
+                selectedSubdomain.SubdomainName = subdomain.SubdomainName;
+                context.SaveChanges();
+            }
+        }
+
+        public static void DeleteSubdomain(int id)
+        {
+            using (var context = new ervinEntities())
+            {
+                var selectedSubdomain = context.Subdomains.FirstOrDefault(x => x.Id == id);
+                context.Subdomains.Remove(selectedSubdomain);
+                context.SaveChanges();
+            }
+
+
+        }
     }
 }
