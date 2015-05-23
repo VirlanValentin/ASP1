34a4cde420d076a64fdd5228d975898de7131a02
VirlanValentin <stefan.virlan@info.uaic.ro>
2015-05-23 17:46:31 +0300

    Crud Question

--- ASPHomework/ASPHomework/Repositories/DSQHelper.cs
+++ ASPHomework/ASPHomework/Repositories/DSQHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using DataBase;
@@ -8,6 +9,7 @@ namespace ASPHomework.Repositories
 {
     public class DSQHelper
     {
+        #region Subdomain
         //SubdomainHelper
         //GetAll
         public static List<Subdomains> GetAllSubdomains()
@@ -38,7 +40,7 @@ namespace ASPHomework.Repositories
                 context.SaveChanges();
             }
         }
-
+        //Delete
         public static void DeleteSubdomain(int id)
         {
             using (var context = new ervinEntities())
@@ -47,8 +49,63 @@ namespace ASPHomework.Repositories
                 context.Subdomains.Remove(selectedSubdomain);
                 context.SaveChanges();
             }
-
-
         }
+        #endregion
+
+        #region Question
+        //GetAll
+        public static List<Questions> GetAllQuestions()
+        {
+            using (var context = new ervinEntities())
+            {
+                return context.Questions.ToList();
+            }
+        }
+        //GetAll qestions of a subdomain
+        public static List<Questions> GetAllQuestionsOfASpecificSubdomain(int id)
+        {
+            using (var context = new ervinEntities())
+            {
+                return context.Questions.Where(x => x.IdSubdomain == id).ToList();
+            }
+        }
+        //Add
+        public static void AddQuestion(Questions question)
+        {
+            using (var context = new ervinEntities())
+            {
+                context.Questions.Add(question);
+                context.SaveChanges();
+            }
+        }
+
+        //Edit
+        public static void EditQuestion(Questions question)
+        {
+            using (var context = new ervinEntities())
+            {
+                var selectedquestion = context.Questions.First(x => x.Id == question.Id);
+                selectedquestion.Question = question.Question;
+                selectedquestion.Answers = question.Answers;
+                selectedquestion.RightAnswer = question.RightAnswer;
+                selectedquestion.Argumentation = question.Argumentation;
+                selectedquestion.TestQuestions = question.TestQuestions;
+                selectedquestion.IdSubdomain = question.IdSubdomain;
+                context.SaveChanges();
+            }
+        }
+
+        //Delete
+        public static void DeleteQuestion(int id)
+        {
+            using (var context = new ervinEntities())
+            {
+                var selectedquestion = context.Questions.FirstOrDefault(x => x.Id == id);
+                context.Questions.Remove(selectedquestion);
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
     }
 }
